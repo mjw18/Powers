eeeb1e1fa3c01c5c65d2d8e5969936fd65cc6559
Mike Weber <mjw18@geneseo.edu>
2015-11-26 00:41:29 -0500

    Just ChargeEffect, shoud rename

---
+++ Assets/Scripts/VisualEffects/ChargeEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeEffect : VisualEffect
+{
+    public ParticleSystem collisionParticles;
+
+    void Awake()
+    {
+
+    }
+
+    public override void PlayEffect()
+    {
+        collisionParticles.Play();
+    }
+}
