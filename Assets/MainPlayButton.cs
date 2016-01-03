5dd5fdd2247c1d9a90e8271c2cf11aa385f23e8c
Mike Weber <mjw18@geneseo.edu>
2016-01-03 18:24:06 -0500

    Added main play button to source.

---
+++ Assets/MainPlayButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainPlayButton : MonoBehaviour {
+
+    private Text buttonText;
+
+    void Awake()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        Application.LoadLevel("main");
+    }
+}
